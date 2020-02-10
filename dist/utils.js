4166c3cc6a79e830b7a93969a4eb3d6eb57ae54e
David Pett <me@davidpett.com>
2020-02-10 15:08:47 -0600

    removed dist, added script

--- dist/utils.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.camelCase = function (str) {
-    return str.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function (match, index) {
-        if (+match === 0)
-            return '';
-        return index == 0 ? match.toLowerCase() : match.toUpperCase();
-    });
-};
